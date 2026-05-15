58090a5fefd11e436b109e08a525bcc13405105d
victo <vdiamar2309@gmail.com>
2026-05-15 11:19:45 +0200

    modified:   src/main/java/Main.java 	new file:   src/main/java/util/MiEntradaSalida.java

--- Arrays/Ejercicio3/src/main/java/Main.java
+++ Arrays/Ejercicio3/src/main/java/Main.java
@@ -1,17 +1,61 @@
-import java.util.Scanner;
+import util.MiEntradaSalida;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
 
 public class Main {
     static void main(String[] args) {
-        so
-    }
+    int [] array = new int[MiEntradaSalida.solicitarEnteroPositivo("Cuantas posiciones quiere que tenga el array?")];
+        insertarNumerosArray(array);
+        double media= (double) Arrays.stream(array).sum()/ array.length;
+
+        System.out.println("La media del array es "+media);
+        System.out.println();
+        numeroMenor(array);
+
+        HashSet<Integer> numeroUnico= extraerNumerosUnicos(array);
+
+        int numeroRepetido;
+        int vecesRepetido;
+        for (int i = 0; i < array.length; i++) {
 
 
-    public static int solicitarEntero(int i){
-        try (Scanner sc = new Scanner(System.in)){
-            i = sc.nextInt();
-        } catch (Exception e){
-            System.out.println(e);
+
+
+
         }
-        return 1; //CAMBIA ESTO
+
+
+
+
     }
+
+    private static HashSet<Integer> extraerNumerosUnicos(int[] array) {
+        HashSet <Integer> numerosUnicos = new HashSet<>();
+        for (int i : array){
+            numerosUnicos.add(i);
+        }
+        return numerosUnicos;
+    }
+
+    private static void numeroMenor(int[] array) {
+        int numeroMenor=Integer.MAX_VALUE;
+        for (int i : array){
+            if (i < numeroMenor){
+                numeroMenor=i;
+            }
+        }
+        System.out.println("El numero menor del array es: "+numeroMenor);
+    }
+
+    private static void insertarNumerosArray(int[] array) {
+        int posicion=1;
+        for(int i =0 ; i < array.length; i++){
+            array[i] =MiEntradaSalida.solicitarEntero("Inserta un numero entero en la posición "+posicion);
+            posicion++;
+        }
+    }
+
+
 }
